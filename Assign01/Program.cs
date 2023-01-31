e2a6d2c9df22d4646860bbd7fc2f87aebbf4f8e7
richgomez21 <123585565+richgomez21@users.noreply.github.com>
2023-01-31 12:12:57 -0600

    Update Program.cs

--- Assign01/Program.cs
+++ Assign01/Program.cs
@@ -1,8 +1,19 @@
 MyList train = new MyList(8);
+train.Append(8);
 train.Append(12);
+train.Append(24);
 
-Console.WriteLine("head value: " + train.Head.Data);
-Console.WriteLine("head value: " + train.Head.Next.Data);
+Node node = train.RemoveFromBack();
+if(node == null){
+Console.WriteLine("null");
+}else{
+    Console.WriteLine("Remove node: " + node.Data)
+}
+
+train.Print();
+
+// Console.WriteLine("head value: " + train.Head.Data);
+// Console.WriteLine("head value: " + train.Head.Next.Data);
 
 // for(int i = 0; i < 10; i++){
 //     train.Append(i);
