58b73d4541a0a2323dc4a87849ec1a3e445afd13
Richard Gomez <gomezr3@students.westerntc.edu>
2023-04-11 12:29:07 -0500

    done

---
+++ example_06_fruit/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using BCrypt.Net;
+
+string filePath = "fruits.json";
+var jsonString = File.ReadAllText(filePath);
+
+Console.WriteLine(jsonString);
+
+var options = new JsonSerializerOptions(){PropertyNameCaseInsensitive = true};
+
+List<Fruit> fruitList = JsonSerializer.Deserialize< List<Fruit> >(jsonString, options);
+
+// loop through fruits and print out: <fruit_name: <fruit_color>
+
+HashSet<string> fruitColors = new HashSet<string>();
+foreach(Fruit fruit in fruitList){
+    fruitColors.Add(fruit.Color);
+};
+
+
+// foreach(Fruit fruit in fruitList){
+//     Console.WriteLine($"{fruit.Name}: {fruit.Color}");
+// };
+
+
+
+
---
+++ example_06_fruit/fruit.cs
@@ -0,0 +1,8 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+public class Fruit{
+    public string Name {get; set;}
+    public string Color {get; set;}
+}
